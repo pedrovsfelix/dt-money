0dc45cd68ec329c00172ec4f20e59475e72d149d
pedrovsfelix <pedrovitorsf@gmail.com>
2025-11-18 20:45:49 -0300

    :sparkles: feat: Adicionando interações com o formulário de busca.

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,12 +1,39 @@
 import { MagnifyingGlassIcon } from "@phosphor-icons/react";
 import { SearchFormContainer } from "./styles";
+import { useForm } from "react-hook-form";
+import * as z from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useContext } from "react";
+
+const searchFormSchema = z.object({
+    query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
-    return (
-        <SearchFormContainer>
-            <input type="text" placeholder="Busque por transações" />
+    const [] = useContext();
 
-            <button type="submit">
+    const { register, handleSubmit, formState: {
+        isSubmitting
+    } } = useForm<SearchFormInputs>({
+        resolver: zodResolver(searchFormSchema),
+    });
+
+    async function handleSearchTransactions(data: SearchFormInputs) {
+        await new Promise(resolve => setTimeout(resolve, 2000))
+        console.log(data)
+    }
+
+    return (
+        <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+            <input 
+                type="text" 
+                placeholder="Busque por transações"
+                {...register('query')}
+            />
+
+            <button type="submit" disabled={isSubmitting}>
                 <MagnifyingGlassIcon size={32} />
                 Buscar
             </button>
