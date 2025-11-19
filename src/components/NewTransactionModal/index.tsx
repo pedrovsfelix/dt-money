286d468760ce65cf2166df92f0962eb99552a88e
pedrovsfelix <seuemail@example.com>
2025-11-19 15:14:25 -0300

    :sparkles: feat: Criando transações para API.

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,8 @@ import { ArrowCircleDownIcon, ArrowCircleUpIcon, X } from "@phosphor-icons/react
 import * as z from 'zod';
 import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 const newTransactionFormSchema = z.object({
   description: z.string(),
@@ -15,18 +17,28 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
+  const { createTransaction } = useContext(TransactionsContext)
   const { 
     control,
     register, 
     handleSubmit,
-    formState: { isSubmitting } 
+    formState: { isSubmitting },
+    reset 
   } = useForm<NewTransactionFormInputs>({
     resolver: zodResolver(newTransactionFormSchema),
   })
 
   async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    console.log(data)
+    const { description, price, category, type } = data;
+
+    await createTransaction({
+      description,
+      price,
+      category,
+      type
+    })
+
+    reset();
   }
 
   return (
